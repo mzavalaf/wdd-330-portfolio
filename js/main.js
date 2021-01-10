d3438e160e37d433367ae065f42feb1e98d79fe8
mzavalaf <35230769+mzavalaf@users.noreply.github.com>
2021-01-09 19:37:13 -0600

    *

--- js/main.js
+++ js/main.js
@@ -3,20 +3,7 @@ const links =
           {
             label: "Week 1",
             url: "week1/index.html"
-              
-          },
           
-          {
-            label: "Week 2",
-            url: "week2/index.html" 
-          },   
-           {
-            label: "Week 3",
-            url: "week3/index.html" 
-          },
-           {
-            label: "Week 4",
-            url: "week4/index.html" 
           } 
       ]
 
