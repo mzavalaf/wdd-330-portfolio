462242863fe385b6891605acc384bd2ab8afdf5d
mzavalaf <35230769+mzavalaf@users.noreply.github.com>
2021-01-21 00:41:13 -0600

    *

--- js/main.js
+++ js/main.js
@@ -7,22 +7,21 @@ const links =
           },
           
           {
-            label: "Local Storage Sample",
-            url: "https://mzavalaf.github.io/wdd-330-portfolio/week1/localstorage.html" 
+            label: "Week 1",
+            url: "week1/index.html" 
           } 
          
       ]
 
-const myList = document.querySelector("week1List");
+const ul = document.querySelector("ul");
 
-for (let i=0; i < links.length; i++) 
-{
-    let listItem = document.createElement("li");
-    let anchor = document.createElement("a");
-    anchor.setAttribute("href", links[i].url);
-    anchor.innerText = links[i].label;
-    listItem.appendChild(anchor);
-    myList.appendChild(listItem);
-
+for (const item of links) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.setAttribute('href', item.url);
+    a.textContent = item.label;
+    li.appendChild(a);
+    
+    ul.appendChild(li);
     }
 
